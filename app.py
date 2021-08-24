7a0685dd437927412ddcc0a3cf5b8b531d47fff6
rbxcoder <rbadaoui01@gmail.com>
2021-08-24 11:42:12 +0100

    v2.1

--- app.py
+++ app.py
@@ -18,17 +18,17 @@ def detect():
     except:
          return jsonify({'message':'no image sent!'})
     try:
-        with open("images/getImage.png", "wb") as fh:
+        with open("getImage.png", "wb") as fh:
             fh.write(base64.b64decode(str(imageb)))
         
     except:
         return jsonify({'message':'error image format!'})
     try:
-        image=cv2.imread('images/getImage.png')
+        image=cv2.imread('getImage.png')
         detected_image = detection(image)
         index=ocr(detected_image)
-        cv2.imwrite('images/postImage.png',detected_image)
-        with open('images/postImage.png', "rb") as image_file:
+        cv2.imwrite('postImage.png',detected_image)
+        with open('postImage.png', "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
         return jsonify({'index':index}) ##'detection':str(encoded_string,'utf-8')
     except:
